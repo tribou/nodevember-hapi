8c4c562da49886d1bb2a1e447da5163d515e6b5d
Aaron Tribou <aaron.tribou@gmail.com>
2014-11-15 14:37:31 -0600

    Testing portion

--- test/index.js
+++ test/index.js
@@ -11,4 +11,17 @@ var describe = lab.describe;
 var it = lab.it;
 var expect = Code.expect;
 
+describe('hello', function() {
 
+    it('replies with hello server', function(done) {
+
+        var server = new Hapi.Server();
+        server.route(Routes);
+
+        server.inject({ method: 'GET', url: '/'}, function(res) {
+
+            expect(res.result).to.equal('Hello Server');
+            done();
+        })
+    })
+});
