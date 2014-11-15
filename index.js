3f4eeefa3dbb7dc4159875a9fa343461a19ac120
Aaron Tribou <aaron.tribou@gmail.com>
2014-11-15 14:17:03 -0600

    Finish first hello_server

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 var Hapi = require('hapi');
 
+var server = new Hapi.Server('localhost', 8080);
 
+server.route({
+    method: 'GET',
+    path: '/hello',
+    handler: function(request, reply) {
+        reply('server');
+    }
+});
+
+server.start(function() {
+
+    console.log("Server started...");
+});
